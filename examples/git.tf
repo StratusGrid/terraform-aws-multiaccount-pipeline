3fe063dbe5a060eebccdfbfb899e9c41032d70f5
Wesley Kirkland <wesley.kirkland@stratusgrid.com>
2022-01-11 12:15:14 -0500

    Version Update

--- examples/git.tf
+++ examples/git.tf
@@ -1,6 +1,6 @@
 module "terraform_pipeline" {
   source  = "StratusGrid/multiaccount-pipeline/aws"
-  version = "~> 2.0.0"
+  version = "~> 3.0.0"
 
   create                             = true
   name                               = "${var.name_prefix}-utils${local.name_suffix}"
